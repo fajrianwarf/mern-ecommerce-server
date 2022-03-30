ce5cf9b9a886a27798817b540d4e0abddb6714ec
fajrianwarf <fajrianwarfachrul@gmail.com>
2022-03-30 20:12:13 +0700

    change database to mongodb atlas

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const { dbAtlasUserName, dbAtlasPass, dbAtlasCluster, dbAtlasName, dbUser, dbPas
 
 const urlAtlas = `mongodb+srv://${dbAtlasUserName}:${dbAtlasPass}@${dbAtlasCluster}.fhypt.mongodb.net/${dbAtlasName}?retryWrites=true&w=majority`;
 const urlLocal = `mongodb://${dbUser}:${dbPass}@localhost:27017/${dbName}?authSource=admin`;
-let connectionDatabase = urlLocal;
+let connectionDatabase = urlAtlas;
 
 // const url2 = 'mongodb+srv://fajrianwarf:<password>@ecommerce-server.fhypt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // console.log(url);
